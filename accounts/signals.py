fee7b9e5f6c2aee90c9106f96ffa5eab099a92e5
Pradip Thapa <codewithpradip.com@gmail.com>
2024-12-01 20:39:46 +0545

    feat(auth): add signal to create user profile on user creation

---
+++ accounts/signals.py
@@ -0,0 +1,16 @@
+from .models import User, UserProfile
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+@receiver(post_save, sender=User)
+def post_save_create_profile_receiver(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        try:
+            profile = UserProfile.objects.get(user=instance)
+            profile.save()
+        except:
+            # Create the userprofile, if not exist
+            UserProfile.objects.create(user=instance)
--- khajaghar_api/settings.py
+++ khajaghar_api/settings.py
@@ -85,6 +85,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -121,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+# Media files configuration
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
